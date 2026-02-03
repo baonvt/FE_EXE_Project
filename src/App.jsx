78d04f3bd80a8d54863ace036fa0450f31a8a791
Nguyễn Võ Thái Bảo <baonguyenvothai@gmail.com>
2026-02-03 11:20:59 +0700

    fix 00

--- src/App.jsx
+++ src/App.jsx
@@ -8,39 +8,36 @@ import CustomerMenuPage from './Customer/pages/CustomerMenuPage'
 import CustomerEntryPage from './Customer/pages/CustomerEntryPage'
 import CustomerNotFoundPage from './Customer/pages/CustomerNotFoundPage'
 import OrderTrackingPage from './Customer/pages/OrderTrackingPage'
-import { ToastProvider } from './context/ToastContext'
 
 function App() {
   return (
-    <ToastProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route
-            path="/bussiness/*"
-            element={
-              <RequireAuth roles={[
-                'restaurant_owner',
-                'restaurant',
-                'owner',
-                'restaurant-admin'
-              ]}>
-                <Bussiness />
-              </RequireAuth>
-            }
-          />
-          <Route path="/onboarding" element={<OnboardingPage />} />
-          
-          {/* Customer QR Scan Routes */}
-          <Route path="/customer" element={<CustomerEntryPage />} />
-          <Route path="/:restaurantSlug/menu/:tableNumber" element={<CustomerMenuPage />} />
-          <Route path="/order/:orderNumber" element={<OrderTrackingPage />} />
-          <Route path="/not-found" element={<CustomerNotFoundPage />} />
-          
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </BrowserRouter>
-    </ToastProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route
+          path="/bussiness/*"
+          element={
+            <RequireAuth roles={[
+              'restaurant_owner',
+              'restaurant',
+              'owner',
+              'restaurant-admin'
+            ]}>
+              <Bussiness />
+            </RequireAuth>
+          }
+        />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+        
+        {/* Customer QR Scan Routes */}
+        <Route path="/customer" element={<CustomerEntryPage />} />
+        <Route path="/:restaurantSlug/menu/:tableNumber" element={<CustomerMenuPage />} />
+        <Route path="/order/:orderNumber" element={<OrderTrackingPage />} />
+        <Route path="/not-found" element={<CustomerNotFoundPage />} />
+        
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
